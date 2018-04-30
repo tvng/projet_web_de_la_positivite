706d05e57427f68e22858e9093034efa110de22a
Rognetta <aureliorognetta@gmail.com>
2018-04-30 14:26:11 +0200

    Connexion
    Connexion

---
+++ pages/connexion.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="connexionTraitement.php" method="POST">
+            <p> Login : <input type="text" name="nom" /></p><br>
+            <p> Mot de passe : <input type="password" name="passw" /></p><br>    
+            <input type="submit" value="Soumettre" name="submit" />
+        </form>
+    </body>
+</html>

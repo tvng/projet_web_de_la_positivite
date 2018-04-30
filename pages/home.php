d81713270f068c94f75d51bfb626434d3b39e621
tvng <37909639+tvng@users.noreply.github.com>
2018-04-30 15:04:01 +0200

    découpage menu-page

--- pages/home.php
+++ pages/home.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-<title>ECE reseau</title>
+	
+	<title>ECE reseau</title>
+	
+	<!-- required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<!-- CSS autre -->
+	<link href="global.css" rel="stylesheet" type="text/css" />
+	
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	<!-- JQuery -->
+  <script src="//code.jquery.com/jquery.min.js"></script>
 </head>
 
 <body>
-<header></header>
+<header>
+  <!-- MENU -->
+  <div>
+    <nav>
+    
+    </nav>
+  </div>
+  <div><?php include ("menu.php"); ?>
+   </div>
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+
+
+
+
+</header>
 
 
 <footer></footer>
---
+++ pages/menu.php
@@ -0,0 +1,6 @@
+<!-- HTML DU MENU  
+    ce code va être "inclus" dans chaque page HTML
+    c'est un seul code pour toutes les pages
+-->
+
+<h3> je suis triste </h3>

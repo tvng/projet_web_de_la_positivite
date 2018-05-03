55801113ce2c2d9c94bf86021a72321d6ba239bc
tvng <37909639+tvng@users.noreply.github.com>
2018-05-03 23:00:47 +0200

    debug publish

--- pages/publish.php
+++ pages/publish.php
@@ -2,10 +2,11 @@
 <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
 <div class="container-fluid">
     <div class="row">
-        <input type="file" name="fileToUpload" id="fileToUpload">
-    </div>
-    <div class="row">
     <textarea id="desc" name="Description" class="form-control" placeholder="Description" required></textarea>
+    </div>
+
+     <div class="row">
+        <input type="file" name="fileToUpload" id="fileToUpload">
     </div>
     <!-- emoji -->
     <div class="row">
@@ -19,7 +20,7 @@
         <input id="place" name="Lieu" type="text" class="form-control col-sm-3" placeholder="Lieu">
     
     </div>
-
+   
     <div class="row">
 
         <label>Visibilité : </label>
@@ -28,10 +29,9 @@
         <option value="Everyone">Public</option>
         <option value="Myself only">Moi uniquement</option>
         </select>
-
+        <input type="submit" class="btn" name="poster" value="poster" />
     </div>
 
-    <input type="submit" class="btn" name="poster" value="poster" />
 </div>
 </form>
 
@@ -60,37 +60,47 @@ function poster() {
         var_dump($_FILES);
         var_dump($_POST);
         */
-        $target_dir = $_SESSION['ID_user']."_uploads/";
-        if (!is_dir($target_dir)){
-            mkdir($target_dir);
+        if (empty($_FILES["fileToUpload"]["tmp_name"])){
+           //no file
+           $target_file=" ";
         }
+        else {
+            echo "no";
+        
+            $target_dir = $_SESSION['ID_user']."_uploads/";
+            if (!is_dir($target_dir)){
+                mkdir($target_dir);
+            }
 
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        //Petit test pour s'assurer qu'on créer le bon fichier
-        //echo $target_file."<br>";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            //Petit test pour s'assurer qu'on créer le bon fichier
+            //echo $target_file."<br>";
 
-        //Tests d'intégrité du fichier (taille, type, non doublon etc)
-        $uploadOk = 1;
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                && $imageFileType != "gif" ) {
-                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            //Tests d'intégrité du fichier (taille, type, non doublon etc)
+            $uploadOk = 1;
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                    && $imageFileType != "gif" ) {
+                    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                    $uploadOk = 0;
+                }
+                if (file_exists($target_file)) {
+                    echo "Sorry, file already exists.";
+                    $uploadOk = 0;
+                }
+            } else {
+                echo "File is not an image.<br>";
                 $uploadOk = 0;
             }
-            if (file_exists($target_file)) {
-                echo "Sorry, file already exists.";
-                $uploadOk = 0;
+
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file) && $uploadOk!== 0) {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
             }
-        } else {
-            echo "File is not an image.<br>";
-            $uploadOk = 0;
         }
 
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file) && $uploadOk!== 0) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        }
+
         $sql="INSERT INTO publication(ID_author, date, time, visibility, location, emotion, text, media_link)
         VALUES ('" . $_SESSION['ID_user'] . "','" . date('Y-m-d') . "','"  . date('h:i:s') . "','" . $_POST['Visible'] . "','" . $_POST['Lieu'] . "','" .
         $_POST['Emotion'] . "','" . $_POST['Description'] . "','" . $target_file . "')";

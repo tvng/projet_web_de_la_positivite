d753a55a55d9617ac7ff389cafbe579b8d0c15e2
Rognetta <aureliorognetta@gmail.com>
2018-05-04 18:07:01 +0200

    deconnexion
    des trucs

--- pages/deconnexion.php
+++ pages/deconnexion.php
@@ -2,6 +2,8 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: connexion.php");
-exit();
+header("localhost/projet_web_de_la_positivite/pages/connexion.php");
+exit;
+//header("Location: connexion.php");
+//exit();
 ?>

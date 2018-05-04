3edcb710c75595845169616a4f1151bc39d32268
tvng <37909639+tvng@users.noreply.github.com>
2018-05-05 01:31:33 +0200

    ajax

--- pages/network_traitement.php
+++ pages/network_traitement.php
@@ -1,15 +1,38 @@
-<?
-session_start();
-?>
 
 <?php
-    //Envoie les demandes de contact de l'utilisateur
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['User_'.$sl_data['ID_user']]))
-    {
-        $sql="INSERT INTO invitation(ID_user1, status1, text, ID_user2, status2) 
-        VALUES('". $_SESSION['ID_user'] . "','" . 1 . "','" . $_POST['text'] . "','" . $sl_data['ID_user'] . "','" . 0 . "')";
+session_start();
+/*
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'Ajouter en ami':
+        
+    echo "The select function is calleddddddddddddddddd.";    
+        add();
+            break;
+    }
+}
+
+function add() {
+    */
+    echo "The select function is called.";
+    echo $_POST['id_user'];
+		
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=eceperanto;charset=utf8', 'root', '');
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        $sql="INSERT INTO invitation (ID_user1, status1, invitation.text, ID_user2, status2) 
+        VALUES('".$_SESSION['ID_user']."', '1', 'hey' ,'".$_POST['id_user']."','0')";
         echo $sql;
         $bdd->exec($sql);
         echo "Demande envoyée!";
-    }
+    
+	//
+//}
+
 ?>

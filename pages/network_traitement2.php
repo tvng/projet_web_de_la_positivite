f78a9857132424c7e1ef25532f53faec0692d295
tvng <37909639+tvng@users.noreply.github.com>
2018-05-05 12:04:59 +0200

    modif vite fait profil

--- pages/network_traitement2.php
+++ pages/network_traitement2.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
 
-  echo "The select function is called.";
-  echo $_POST['id_user'];
-      
       try
       {
           $bdd = new PDO('mysql:host=localhost;dbname=eceperanto;charset=utf8', 'root', '');
@@ -15,7 +12,6 @@ session_start();
 
       $sql="INSERT INTO invitation (ID_user1, status1, invitation.text, ID_user2, status2) 
       VALUES('".$_SESSION['ID_user']."', '1', '".$_POST['txt']."' ,'".$_POST['id_user']."','0')";
-      echo $sql;
       $bdd->exec($sql);
       echo "Demande envoyée!";
 ?>
